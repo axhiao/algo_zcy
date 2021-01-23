3f25943da1676bfa0fe9ae96a53ebf6c51ad982e
Minglei Yin <axhiao@gmail.com>
2021-01-23 16:10:24 -0500

    preOrder and inOrder traverse binary tree

---
+++ src/main/java/com/axhiao/aer/ch3/BinaryTreeRecursiveTraverse.java
@@ -0,0 +1,99 @@
+package com.axhiao.aer.ch3;
+
+import java.util.Stack;
+
+public class BinaryTreeRecursiveTraverse {
+
+
+    public static void preOrderRecurr(Node head) {
+        if (head == null)
+            return;
+        System.out.print(head.value + " ");
+        preOrderRecurr(head.left);
+        preOrderRecurr(head.right);
+    }
+
+    public static void inOrderRecurr(Node head) {
+        if (head == null)
+            return;
+        inOrderRecurr(head.left);
+        System.out.print(head.value + " ");
+        inOrderRecurr(head.right);
+    }
+
+    public static void posOrderRecurr(Node head) {
+        if (head == null)
+            return;
+        posOrderRecurr(head.left);
+        posOrderRecurr(head.right);
+        System.out.print(head.value + " ");
+    }
+
+    public static void preOrderNonRecurr(Node head) {
+        if (head == null)
+            return;
+        Stack<Node> stack = new Stack<>();
+        stack.push(head);
+        while(!stack.empty()) {
+            Node curr = stack.pop();
+            System.out.print(curr.value + " ");
+            if (curr.right != null)
+                stack.push(curr.right);
+            if (curr.left != null)
+                stack.push(curr.left);
+        }
+    }
+
+    public static void inOrderNonRecurr(Node head) {
+        Stack<Node> stack = new Stack<>();
+        Node curr = head;
+        while(!stack.empty() || curr != null) {
+            if (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            } else {
+                curr = stack.pop();
+                System.out.print(curr.value + " ");
+                curr = curr.right;
+            }
+        }
+    }
+
+    public static Node buildTree() {
+        /*
+                     1
+                  /    \
+                 2      3
+                /      / \
+               4       5  6
+                \
+                 7
+        * */
+        Node h1 = new Node(1);
+        Node h2 = new Node(2);
+        Node h3 = new Node(3);
+        Node h4 = new Node(4);
+        Node h5 = new Node(5);
+        Node h6 = new Node(6);
+        Node h7 = new Node(7);
+        h1.left = h2; h1.right = h3;
+        h2.left = h4;
+        h3.left = h5; h3.right = h6;
+        h4.right = h7;
+        return h1;
+    }
+
+    public static void main(String[] args) {
+        Node tree = buildTree();
+        System.out.println("pre-Order Traverse:");
+        preOrderRecurr(tree);
+        System.out.println();
+        preOrderNonRecurr(tree);
+        System.out.println("\nin-Order Traverse:");
+        inOrderRecurr(tree);
+        System.out.println();
+        inOrderNonRecurr(tree);
+        System.out.println("\npost-Order Traverse:");
+        posOrderRecurr(tree);
+    }
+}
---
+++ src/main/java/com/axhiao/aer/ch3/Node.java
@@ -0,0 +1,11 @@
+package com.axhiao.aer.ch3;
+
+public class Node {
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int v) {
+        this.value = v;
+    }
+}
